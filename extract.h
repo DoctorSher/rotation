55edd22b831d02da9798ae435a3f85143f67ad56
doctorsher <adam.r.drescher@gmail.com>
2013-10-25 17:08:10 -0500

    Started adding signature information

--- extract.h
+++ extract.h
@@ -90,6 +90,147 @@ struct field {
  * 0,1,0,1,1,0,0,0,0,4,0,3,0,0,0,2,0,1,.... 
  */
 
+/* 10.9.1 PERSISTENT CONNECTIONS */
+
+#define ACCEPT 0
+#define ACCEPT_CHARSET 1
+#define ACCEPT_ENCODING 2
+#define ACCEPT_LANGUAGE 3
+#define ALLOW 4
+#define AUTHORIZATION 5
+#define BASE 6
+#define CACHE_CONTROL 7
+#define CONNECTION 8
+#define CONTENT_ENCODING 9
+#define CONTENT_LANGUAGE 10
+#define CONTENT_LENGTH 11
+#define CONTENT_MD5 12
+#define CONTENT_RANGE 13
+#define CONTENT_TYPE 14
+#define CONTENT_VERSION 15
+#define DATE 16
+#define DERIVED_FROM 17
+#define EXPIRES 18
+#define FORWARDED 19
+#define FROM 20
+#define HOST 21
+#define IF_MODIFIED_SINCE 22
+#define KEEP_ALIVE 23
+#define LAST_MODIFIED 24
+#define LINK 25
+#define LOCATION 26
+#define MIME_VERSION 27
+#define PRAGMA 28
+#define PROXY_AUTHENTICATE 29
+#define PROXY_AUTHORIZATION 30
+#define PUBLIC 31
+#define RANGE 32
+#define REFERER 33
+#define REFRESH 34
+#define RETRY_AFTER 35
+#define SERVER 36
+#define TITLE 37
+#define TRANSFER_ENCODING 38
+#define UNLESS 39
+#define UPGRADE 40
+#define URI 41
+#define USER_AGENT 42
+#define WWW_AUTHENTICATE 43
+
+#define HTTP_MAX_FIELDS 44
+
+ /* ACCEPT */
+ /* ACCEPT_CHARSET */
+ /* ACCEPT_ENCODING */
+ /* ACCEPT_LANGUAGE */
+ /* ALLOW */
+ /* AUTHORIZATION */
+ /* BASE */
+ /* CACHE_CONTROL */
+ /* CONNECTION */
+ /* CONTENT_ENCODING */
+ /* CONTENT_LANGUAGE */
+ /* CONTENT_LENGTH */
+ /* CONTENT_MD5 */
+ /* CONTENT_RANGE */
+ /* CONTENT_TYPE */
+ /* CONTENT_VERSION */
+ /* DATE */
+ /* DERIVED_FROM */
+ /* EXPIRES */
+ /* FORWARDED */
+ /* FROM */
+ /* HOST */
+ /* IF_MODIFIED_SINCE */
+ /* KEEP_ALIVE */
+ /* LAST_MODIFIED */
+ /* LINK */
+ /* LOCATION */
+ /* MIME_VERSION */
+ /* PRAGMA */
+ /* PROXY_AUTHENTICATE */
+ /* PROXY_AUTHORIZATION */
+ /* PUBLIC */
+ /* RANGE */
+ /* REFERER */
+ /* REFRESH */
+ /* RETRY_AFTER */
+ /* SERVER */
+ /* TITLE */
+ /* TRANSFER_ENCODING */
+ /* UNLESS */
+ /* UPGRADE */
+ /* URI */
+ /* USER_AGENT */
+ /* WWW_AUTHENTICATE */
+
+
+/* #define Accept  */
+/* #define Accept_Charset */
+/* #define Accept_Encoding */
+/* #define Accept_Language */
+/* #define Allow */
+/* #define Authorization */
+/* #define Base */
+/* #define Cache_Control */
+/* #define Connection */
+/* /\* 10.9.1 Persistent Connections *\/ */
+/* #define Content_Encoding */
+/* #define Content_Language */
+/* #define Content_Length */
+/* #define Content_MD5 */
+/* #define Content_Range */
+/* #define Content_Type */
+/* #define Content_Version */
+/* #define Date */
+/* #define Derived_From */
+/* #define Expires */
+/* #define Forwarded */
+/* #define From */
+/* #define Host */
+/* #define If_Modified_Since */
+/* #define Keep_Alive */
+/* #define Last_Modified */
+/* #define Link */
+/* #define Location */
+/* #define MIME_Version */
+/* #define Pragma */
+/* #define Proxy_Authenticate */
+/* #define Proxy_Authorization */
+/* #define Public */
+/* #define Range */
+/* #define Referer */
+/* #define Refresh */
+/* #define Retry_After */
+/* #define Server */
+/* #define Title */
+/* #define Transfer_Encoding */
+/* #define Unless */
+/* #define Upgrade */
+/* #define URI */
+/* #define User_Agent */
+/* #define WWW_Authenticate */
+
 
 #endif /* EXTRACT_H */
 
